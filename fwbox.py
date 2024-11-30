de52efe760b1d91e381683f0a5edc6bbfa0670a5
Josuah Demangeon <me@josuah.net>
2024-11-30 20:11:13 +0100

    initial REPL using python's cmd library
    This has the concept of runners that can execute commands on one or
    several remote host(s), all controlled from the local machine.
    
    This permits implementation of a "runner" that handles command done
    either locally either remotely.

--- fwbox.py
+++ fwbox.py
@@ -1,56 +1,199 @@
-import cmd, sys
+# Copyright (c) 2024 Panoramix Labs
+# SPDX-License-Identifier: MIT
+
+# This software is a simple script with no dependency for ease of
+# deployment, i.e. embedded Linux board with python pre-shipped
+
+import cmd, sys, subprocess, socket
 
 
-class FwBoxShell(cmd.Cmd):
-    intro = 'Welcome to the fwbox shell. Type "help" or "?" to list commands.\n'
-    prompt = "fwbox>>> "
-    file = None
+RED = '\x1b[1;31m'
+GREEN = '\x1b[1;32m'
+YELLOW = '\x1b[1;33m'
+BLUE = '\x1b[1;34m'
+END = '\x1b[m'
 
-    # Actual commands
 
-    def do_test(self, arg):
-        "Test command"
-        print(*parse(arg))
+class Platform:
+    '''Context where to run the shell commands: locally, on a remote system...'''
 
-    # Internal operation
+    all = {}
 
-    def do_quit(self, arg):
-        "Stop recording and exit"
-        self.close()
+    def __init__(self, name: str):
+        self.name = name
+        self.all[str(self)] = self
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def run(self, *args):
+        '''Run the given command on the current platform'''
+        raise NotImplementedError('scan not implemented for this runner')
+
+class LocalPlatform(Platform):
+    '''Local command execution'''
+
+    def run(self, *args):
+        print('$ ' + ' '.join(args))
+        return subprocess.run(args, stdout=subprocess.PIPE)
+
+
+class SshPlatform(Platform):
+    '''Local command execution'''
+
+    def run(self, *args):
+        args = ('ssh', '-oControlMaster=auto', self.name) + args
+        print('$ ' + ' '.join(args))
+        return subprocess.run(args, stdout=subprocess.PIPE)
+
+
+class Runner:
+    '''Handler for a subset of commands'''
+
+    types = list()
+    all = {}
+    command = None
+
+    def __init__(self, name: str, platform: Platform):
+        self.name = name
+        self.channels = list()
+        self.platform = platform
+        self.all[str(self)] = self
+
+    def __str__(self):
+        return type(self).str(self.name, self.platform)
+
+    @classmethod
+    def scan(cls, platform: Platform) -> list[str]:
+        '''Return a list of strings corresponding to devices on this platform.'''
+        raise NotImplementedError('scan not implemented for this runner')
+
+    @classmethod
+    def str(cls, name: str, platform: Platform) -> str:
+        return f'{platform}:{cls.__name__}.{name}'
+
+    @classmethod
+    def add(cls, name: str, platform: Platform):
+        '''Register the runner to the list of all runners if it does not exist yet.'''
+        id = cls.str(name, platform)
+        if id not in cls.all:
+            cls.all[id] = cls(name, platform)
+
+    def ping(self) -> bool:
+        '''Return a whether the connection with the runner is alive.'''
+        return False
+
+    def run(self, *args):
+        '''Run the runner command on the current platform.'''
+        return self.platform.run(*self.command, *args)
+
+
+class DemoRunner(Runner):
+    '''
+    Emulated runner without actual hardware attached to it
+    '''
+
+    @classmethod
+    def scan(cls, platform: Platform) -> list[str]:
+        return ['test',]
+
+    def ping(self) -> bool:
         return True
 
-    def do_eof(self, arg):
-        'Called when hitting Ctrl+D, same as the "quit" command'
-        print("")
-        return self.do_quit(arg)
+Runner.types.append(DemoRunner)
 
-    # Record/playback-related operations
 
-    def do_record(self, arg):
-        "Save future commands to /tmp/fwbox.cmd"
-        self.file = open("/tmp/fwbox.cmd", "w")
+class SigrokRunner(Runner):
+    '''
+    Runner for Sigrok command line back-end: sigrok-cli.
+    https://sigrok.org/
+    '''
 
-    def do_playback(self, arg):
-        "Playback commands from /tmp/fwbox.cmd"
-        self.close()
-        with open("/tmp/fwbox.cmd") as f:
-            self.cmdqueue.extend(f.read().splitlines())
+    command = ('sigrok-cli',)
 
-    def precmd(self, line):
-        line = line.lower()
-        if self.file and "playback" not in line:
-            print(line, file=self.file)
-        return line
+    def __init__(self, name: str, platform: Platform):
+        super(SigrokRunner, self).__init__(name, platform)
+
+        if ':' not in name:
+            self.driver = name
+            self.config = ''
+        else:
+            self.driver, self.config = name.split(':')
+
+        self.command = self.command + ('--driver', self.driver, '--config', self.config)
+
+        stdout = self.run('--show').stdout
+        for line in stdout.decode('utf8').split('\n'):
+            words = line.split(' ')
+            if words[0:6] == ['', '', '', '', 'Logic:', 'channels']:
+                self.channels.extend(words[6:])
+
+    @classmethod
+    def scan(cls, platform: Platform) -> list[str]:
+        '''list all sigrok devices attached to this platform'''
+
+        stdout = platform.run(*cls.command, '--scan').stdout
+        for line in stdout.decode('utf8').split('\n'):
+            fields = line.split(' - ')
+            if len(fields) > 1:
+                yield fields[0]
+
+    def ping(self) -> bool:
+        return self.run('--show').returncode == 0
+
+Runner.types.append(SigrokRunner)
+
+
+class Shell(cmd.Cmd):
+    intro = '''-- Shell ready. Type 'help' or '?' to list commands.'''
+    prompt = f'{BLUE}fwbox${END} '
+
+    def parse(self, arg: str):
+        return tuple(arg.split())
 
     def close(self):
-        if self.file:
-            self.file.close()
-            self.file = None
+        pass
+
+    def preloop(self):
+        print('''-- Scanning all runners...''')
+        self.do_refresh('')
+
+    def do_refresh(self, arg: str):
+        '''Open all available runners'''
+
+        for _, platform in Platform.all.items():
+            print(f'-- platform {platform}')
+
+            for cls in Runner.types:
+                print(f'-- scannning for {cls.__name__} on {platform}')
+
+                for name in cls.scan(platform):
+                    cls.add(name, platform)
+
+    def do_ssh(self, arg: str):
+        '''Add an ssh host to the list of platforms'''
+        SshPlatform(arg)
+        self.do_refresh('')
+
+    def do_forget(self, arg: str):
+        '''Reset the list of connected runners'''
+
+        global runners
+        runners = {}
+
+    def do_list(self, arg: str):
+        '''List all runners to check if they are still connected'''
+
+        for name, runner in Runner.all.items():
+            res = f'{GREEN}OK{END}' if runner.ping() else f'{RED}ERR{END}'
+            print(f'[{res}] {name}: {runner.channels}')
+
+    def do_EOF(self, arg):
+        '''Called when hitting Ctrl+D to quit'''
+        print('-- quitting fwbox shell')
+        return self.do_quit(arg)
 
 
-def parse(arg):
-    return tuple(map(int, arg.split()))
-
-
-if __name__ == "__main__":
-    FwBoxShell().cmdloop()
+if __name__ == '__main__':
+    LocalPlatform(socket.gethostname())
+    Shell().cmdloop()
