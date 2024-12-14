47f898a1e77baf8e34f9dd0c3fdfe0b8611aba4c
Josuah Demangeon <me@josuah.net>
2024-12-14 04:15:06 +0000

    zephyr: enable all board built-in functions

--- zephyr/app/src/main.c
+++ zephyr/app/src/main.c
@@ -1,18 +1,45 @@
 /*
- * Copyright (c) 2024 Panoramix Labs
+ * Copyright (c) 2019 Intel Corporation
+ *
  * SPDX-License-Identifier: Apache-2.0
  */
 
+/**
+ * @file
+ * @brief Sample echo app for CDC ACM class
+ *
+ * Sample app for USB CDC ACM class driver. The received data is echoed back
+ * to the serial port.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/uart.h>
 #include <zephyr/kernel.h>
-#include <zephyr/shell/shell.h>
+#include <zephyr/sys/ring_buffer.h>
+
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/usb/usbd.h>
 #include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(cdc_acm_echo, LOG_LEVEL_INF);
 
-#include <app_version.h>
-
-LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+const struct device *const uart_dev = DEVICE_DT_GET_ONE(zephyr_cdc_acm_uart);
 
 int main(void)
 {
+	int ret;
+
+	if (!device_is_ready(uart_dev)) {
+		LOG_ERR("CDC ACM device not ready");
+		return 0;
+	}
+
+	ret = usb_enable(NULL);
+	if (ret != 0) {
+		LOG_ERR("Failed to enable USB");
+		return 0;
+	}
+
 	return 0;
 }
-
